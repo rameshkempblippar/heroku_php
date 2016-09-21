48efbdd758afd129302121b7f95c7df686b4e49d
Ramesh Kempanna <ramesh.kempanna@blippar.com>
2016-09-21 12:44:08 +0530

    Updated database file

--- doc_root/database.php
+++ doc_root/database.php
@@ -7,18 +7,31 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
-$res = $conn->query("SELECT * from TABLES");
+
+if ($conn->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error;
+}
+
+if (!$conn->query("DROP TABLE IF EXISTS test") ||
+    !$conn->query("CREATE TABLE test(id INT, label CHAR(1))") ||
+    !$conn->query("INSERT INTO test(id, label) VALUES (1, 'a')")) {
+    echo "Table creation failed: (" . $conn->errno . ") " . $conn->error;
+}
+
+$res = $conn->query("SELECT * from test");
 echo "Reverse order...\n";
 for ($row_no = $res->num_rows - 1; $row_no >= 0; $row_no--) {
     $res->data_seek($row_no);
     $row = $res->fetch_assoc();
     echo " id = " . $row['id'] . "\n";
+    echo " label = " . $row['label'] . "\n";
 }
 
 echo "Result set order...\n";
 $res->data_seek(0);
 while ($row = $res->fetch_assoc()) {
     echo " id = " . $row['id'] . "\n";
+    echo " label = " . $row['label'] . "\n";
 }
 
 ?>
