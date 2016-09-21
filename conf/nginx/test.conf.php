e050243412f2e2e79465178691cae7d97f5f0afc
rameshkempblippar <ramesh.kempanna@blippar.com>
2016-09-21 10:52:59 +0530

    updated config

--- conf/nginx/test.conf.php
+++ conf/nginx/test.conf.php
@@ -1,3 +1,12 @@
+location / {
+    index  index.php index.html index.htm;
+}
+
+# for people with app root as doc root, restrict access to a few things
+location ~ ^/(composer\.|Procfile$|<?=getenv('COMPOSER_VENDOR_DIR')?>/|<?=getenv('COMPOSER_BIN_DIR')?>/) {
+    deny all;
+}
+
 location ~ ^/test/ {
-   rewrite ^/test/(.*) /test_root/$1 last;
+   rewrite ^/test/(.*) $scheme://$server_name/test_root/$1 last;
 }
